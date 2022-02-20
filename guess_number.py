992ee4804d8df47c85940f0e4ca3c207e30281f1
Yuzhe Deng <g0922884270@gmail.com>
2022-02-21 05:32:28 +0800

    second version

--- guess_number.py
+++ guess_number.py
@@ -2,13 +2,16 @@ import random
 
 r= random.randint(1,100)
 
+y=0
 
 
 while True:
 	x=input('請輸入數字:')
+	y=y+1
 	x=int(x)
 	if x == r :
 		print('終於猜對了')
+		print('總共猜了', y ,'次')
 		break
 
 	elif x > r :
@@ -16,3 +19,4 @@ while True:
 	
 	elif x < r :
 		print('比答案小')
+	print('這是你猜的第', y ,'次')
